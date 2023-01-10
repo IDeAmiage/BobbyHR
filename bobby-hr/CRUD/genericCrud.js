5ef2122f080b4682233fcf9516559feb4d58e3c1
PPC <pierre.paum.cavallera@gmail.com>
2023-01-10 16:16:22 +0100

    link crud to bd

--- bobby-hr/CRUD/genericCrud.js
+++ bobby-hr/CRUD/genericCrud.js
@@ -3,27 +3,27 @@ import PocketBase from 'pocketbase';
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 
-async function create_(pb, table, id, dict){
+export async function create_(pb, table, dict){
     const record1 = await pb.collection(table).create(id, 
         dict
         );
     return record1;
 }
 
-async function delete_(pb, table, id){
+export async function delete_(pb, table, id){
     const record1 = await pb.collection(table).delete(id
         );
     return record1;
 }
 
-async function update_(pb, table, id, dict){
+export async function update_(pb, table, id, dict){
     const record1 = await pb.collection(table).update(id, 
         dict
         );
     return record1;
 }
 
-async function read_(pb, table, id){
+export async function read_(pb, table, id){
     const record1 = await pb.collection(table).getOne(id, 
         {}
         );
