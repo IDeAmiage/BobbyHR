ffb8a0529eee61665b9bc74d3ddda3d9ec3721a2
Merlin <merlincro@hotmail.fr>
2023-01-11 14:53:54 +0100

    update crud

--- bobby-hr/CRUD/genericCrud.js
+++ bobby-hr/CRUD/genericCrud.js
@@ -1,31 +1,17 @@
-import PocketBase from 'pocketbase';
+import pb from '../app/util/pocketbase'
 
-const pb = new PocketBase('http://127.0.0.1:8090');
-
-
-export async function create_(pb, table, dict){
-    const record1 = await pb.collection(table).create(id, 
-        dict
-        );
-    return record1;
+export async function create_(table, dict){
+    return pb.collection(table).create(dict);
 }
 
-export async function delete_(pb, table, id){
-    const record1 = await pb.collection(table).delete(id
-        );
-    return record1;
+export async function delete_(table, id){
+    return await pb.collection(table).delete(id);
 }
 
-export async function update_(pb, table, id, dict){
-    const record1 = await pb.collection(table).update(id, 
-        dict
-        );
-    return record1;
+export async function update_(table, id, dict){
+    return await pb.collection(table).update(id, dict);
 }
 
-export async function read_(pb, table, id){
-    const record1 = await pb.collection(table).getOne(id, 
-        {}
-        );
-    return record1;
+export async function read_(table, id){
+    return await pb.collection(table).getOne(id, {});
 }
