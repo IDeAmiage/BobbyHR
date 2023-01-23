266c1631991fe1828ae4a77375f17df89b88af0f
Augustin Laurent <augustin@MacBook-Pro-de-Augustin.local>
2023-01-23 15:47:15 +0100

    Finalisation page de login

--- bobby-hr/tailwind.config.js
+++ bobby-hr/tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -6,7 +10,30 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white_background_bobby: "#FFFCF9",
+        white_accent_bobby: "#E9E6E4",
+        gray_bobby: "#CACACA",
+        black_bobby: "#121212",
+        brown_bobby: "#63535B"
+      },
+      fontFamily: {
+        Playfair: ["Playfair Display"]
+       }
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    }
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms')
+  ],
 }
