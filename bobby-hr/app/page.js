d6be94fb4491bb54751870f5267a563df183dc02
Augustin Laurent <augustin.lrt2@icloud.com>
2023-01-24 15:12:43 +0100

    KEK

--- bobby-hr/app/page.js
+++ bobby-hr/app/page.js
@@ -1,14 +1,17 @@
 import Link from 'next/link';
 import './globals.css'
-import 'feather-icons'
 
 export default function HomePage() {
-    return (
-        <main>
-            <h1 className="text-3xl underline">
-                Hello world!
-            </h1>
-            <i data-feather="user"></i>
-        </main>
-    );
+  return (
+      <main>
+          <nav>
+              <Link href="/">Home</Link>
+              <Link href="/login">Login</Link>
+              <Link href="/auth">Auth</Link>
+          </nav>
+          <h1 className="text-3xl underline">
+              Hello world!
+          </h1>
+      </main>
+  );
 }
