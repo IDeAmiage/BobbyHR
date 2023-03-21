d956033adcf1c622fac0c8383ab578bcb49a6ec3
PPC <pierre.paum.cavallera@gmail.com>
2023-03-21 14:16:29 +0100

    init branch

--- bobby-hr/app/page.js
+++ bobby-hr/app/page.js
@@ -1,7 +1,10 @@
 import Link from 'next/link';
 import './globals.css'
+import Image from 'next/image'
+import logo from './/assets/logo2.png'
 
 export default function HomePage() {
+  
   return (
       <main>
           <nav>
@@ -11,9 +14,14 @@ export default function HomePage() {
               <Link href="/choice">Choix</Link>
               <Link href="/cv">CV</Link>
           </nav>
-          <h1 className="text-3xl underline">
-              Hello world!
-          </h1>
+          <div className='flex justify-center'>
+            <Image className='h-35 rounded-2xl w-40 bg-white_accent_bobby ' src={logo} alt="logo"/>
+        </div>
+        <div className='grid justify-items-center mt-5'>
+            <h1 className="text-5xl font-bold font-serif mb-3"> IDeA </h1>
+            <h2 className='text-4xl font-medium font-serif '>Data & Compute Science</h2>
+            <h5 className='text-2xl font-serif font-extralight text-gray_bobby'>Startup toulousaine spécialisée dans le SI et traitement de données</h5>
+        </div> 
       </main>
   );
 }
