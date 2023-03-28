255efb8fab500606fec0c2227c309beb32499b55
Merlin <merlincro@hotmail.fr>
2023-03-28 17:14:06 +0200

    [UPD] Amélioration de l'ui du choix des rôles avec tailwind

--- bobby-hr/app/forms/(roles)/page.js
+++ bobby-hr/app/forms/(roles)/page.js
@@ -1,31 +1,96 @@
-export default function RoleForm() {
-    return <form>
-      <h2>Saisie des Rôles</h2>
-      <label>Role:</label><br></br><br></br>
-      <input type="text" id="frole" name="frole" value="Developpeur"/><br></br><br></br>
-      <label>Projet:</label><br></br><br></br>
-      {/* <input type="text" id="box" name="comboboxbox"/> */}
-      <select name="list" onChange="combo(this, 'box)">
-          <option>option 1</option>
-          <option>option 2</option>
-          <option>option 3</option>
-          <option> option 4 </option>
-          <option> option 5 </option>
-      </select><br></br><br></br>
-      <label>Nombre de M1:</label><br></br><br></br>
-      <input type="text" id="fm1" name="fm1" value="2"/><br></br><br></br>
-      <label>Nombre de M2:</label><br></br><br></br>
-      <input type="text" id="fm2" name="fm2" value="2"/><br></br><br></br>
-      <input type="submit" value="Submit"/>
-    </form>;
-  }
+'use client';
 
-  //Avec rôle, projet et nombre de M1 & M2 attendu
+import {useForm} from "react-hook-form";
+import "../../globals.css";
 
-  // <form>
-  //   <label for="fname">First name:</label><br>
-  //   <input type="text" id="fname" name="fname" value="John"><br>
-  //   <label for="lname">Last name:</label><br>
-  //   <input type="text" id="lname" name="lname" value="Doe"><br><br>
-  //   <input type="submit" value="Submit">
-  //   </form>
+function RoleForm() {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    console.log(errors);
+    async function onSubmit(data) {
+        //login({email: data.email, password: data.password});
+        console.log(data);
+        reset();
+    }
+
+    const users = [
+        {
+            name: "Eddie DePetbro",
+            picture: "https://fancytailwind.com/static/profile10-9e05bd5638c669c34c11cb0462d95aa9.jpg",
+            role: "Développeur",
+            online: true,
+            link: "#userProfile1"
+        },
+        {
+            name: "Eddie DePetbro",
+            picture: "https://fancytailwind.com/static/profile10-9e05bd5638c669c34c11cb0462d95aa9.jpg",
+            role: "Scrum Master",
+            online: true,
+            link: "#userProfile1"
+        }
+    ]
+
+    return (
+    <>
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col min-h-screen justify-start items-center bg-white_background_bobby">
+            <div className="py-8">
+                <h1 className="text-7xl font-Playfair font-extrabold text-black_bobby">Choix rôles</h1>
+            </div>
+            <select {...register("Projet", { required: true })}>
+                <option value="EMC">EMC</option>
+            </select>
+            <ul className="flex flex-col">
+                {users.map(user => (
+                    <li key={user.role} className="border-b-2 border-gray-100">
+                        <div className={`py-5 px-4 flex justify-start border-transparent bg-transparent gap-4 items-center`}>
+                            <div className={``}>
+                                {user.role}
+                            </div>
+                            <div className="sm:col-span-2 sm:col-start-1">
+                                <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Nombre de M1
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        type="number"
+                                        name="city"
+                                        {...register(user.role + "M1", {})}
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+
+                            <div className="sm:col-span-2">
+                                <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                                    Nombre de M2
+                                </label>
+                                <div className="mt-2">
+                                    <input
+                                        type="number"
+                                        name="city"
+                                        {...register(user.role + "M2", {})}
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                ))
+                }
+            </ul>
+            <button type="submit">Envoyer</button>
+        </form>
+    </>);
+}
+
+export default RoleForm;
+
+//Avec rôle, projet et nombre de M1 & M2 attendu
+
+// <form>
+//   <label for="fname">First name:</label><br>
+//   <input type="text" id="fname" name="fname" value="John"><br>
+//   <label for="lname">Last name:</label><br>
+//   <input type="text" id="lname" name="lname" value="Doe"><br><br>
+//   <input type="submit" value="Submit">
+//   </form>
--- bobby-hr/app/page.js
+++ bobby-hr/app/page.js
@@ -9,6 +9,7 @@ export default function HomePage() {
               <Link href="/auth">Auth</Link>
               <Link href="/choice">Choix</Link>
               <Link href="/cv">CV</Link>
+              <Link href="/forms">Roles</Link>
           </nav>
           <h1 className="text-3xl underline">
               Hello world!
