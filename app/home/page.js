e72d2f07f8d2a633eb7003711719fa7b8930b600
Merlin Croain <merlincro@hotmail.fr>
2023-05-11 10:06:28 +0200

    Better home page

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState, useEffect } from "react";
-import { Transition } from "@headlessui/react";
-import { Menu, ArrowDown } from "react-feather";
-import { Player } from "@lottiefiles/react-lottie-player";
-import { Parallax, ParallaxLayer, IParallax } from "@react-spring/parallax";
-import { config } from "@react-spring/web";
+import {useState, useEffect} from "react";
+import {Transition} from "@headlessui/react";
+import {Menu, ArrowDown} from "react-feather";
+import {Player} from "@lottiefiles/react-lottie-player";
+import {Parallax, ParallaxLayer, IParallax} from "@react-spring/parallax";
+import {config} from "@react-spring/web";
 
 import Image from "next/image";
 import Logo from "@/public/BusinessTeam.json";
@@ -15,7 +15,7 @@ import puydufou from "@/public/image/puydufou.jpg";
 import SliderHome from "components/slider-home";
 
 import styles from "./home.module.css";
-import { Card } from "@/components/card";
+import {Card} from "@/components/card";
 import TextTransition from "@/components/text-transition";
 
 // PixiJS
@@ -58,8 +58,81 @@ export default function Example() {
   return (
     <>
       <Parallax pages={2} className="bg-white_background_bobby">
-        <ParallaxLayer sticky={{ start: 0, end: 3 }} className="relative">
-          <div className="absolute right-0 top-0 h-20 w-20 rounded-bl-full bg-brown_bobby" />
+
+        <ParallaxLayer
+          offset={0.05}
+          speed={1}
+          className="flex flex-col items-center"
+        >
+          <h1 className="font-Playfair text-9xl font-extrabold text-black_bobby">
+            IDEA
+          </h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={1}
+          className="flex flex-col items-center"
+        >
+          <div className="flex flex-row gap-2">
+            <div>A company to help you to</div>
+            <h1>
+              <TextTransition springConfig={config.wobbly}>
+                {text[index % text.length]}
+              </TextTransition>
+            </h1>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.1}
+          speed={1}
+        >
+          <div>
+            <Player
+              autoplay
+              loop
+              src={Logo}
+              className="w-1/3"
+            />
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.8}
+          speed={1}
+          className="flex flex-col items-center"
+        >
+          <div>EXPLORE</div>
+          <ArrowDown className="h-8 w-8" aria-hidden="true"/>
+        </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1.05}
+            speed={0.8}
+            style={{padding: "0 0 0 20%"}}
+          >
+            <Card className="w-1/3">{projects[0]}</Card>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1.35}
+            speed={1.2}
+            style={{padding: "0 0 0 60%"}}
+          >
+            <Card className="w-1/3">{projects[1]}</Card>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={1.65}
+            speed={1}
+            style={{padding: "0 0 0 40%"}}
+          >
+            <Card className="w-1/3">{projects[2]}</Card>
+          </ParallaxLayer>
+
+        <ParallaxLayer sticky={{start: 0, end: 3}} className="relative">
+          <div className="absolute right-0 top-0 h-20 w-20 rounded-bl-full bg-brown_bobby"/>
           <div className="absolute right-0 top-0 pr-4 pt-4">
             <div className="flex items-start justify-end">
               <button
@@ -68,61 +141,14 @@ export default function Example() {
                 onClick={() => setOpen(!open)}
               >
                 <span className="sr-only">Open panel</span>
-                <Menu className="h-8 w-8" aria-hidden="true" />
+                <Menu className="h-8 w-8" aria-hidden="true"/>
               </button>
             </div>
           </div>
         </ParallaxLayer>
-
-        <ParallaxLayer
-          offset={0}
-          speed={1}
-        >
-          <div className="flex h-screen flex-col items-center">
-            <h1 className="pt-8 font-Playfair text-9xl font-extrabold text-black_bobby">
-              IDEA
-            </h1>
-            <div className="flex flex-row gap-2 pt-6">
-              <div>A company to help you to</div>
-              <h1>
-                <TextTransition springConfig={config.wobbly}>
-                  {text[index % text.length]}
-                </TextTransition>
-              </h1>
-            </div>
-            <div>
-              <Player
-                autoplay
-                loop
-                src={Logo}
-                className="h-96 w-96"
-              />
-            </div>
-            <div>EXPLORE</div>
-            <ArrowDown className="h-8 w-8" aria-hidden="true" />
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer
-            offset={1}
-            speed={1}
-          >
-            <Card>{projects[0]}</Card>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={1.4}
-            speed={1}
-          >
-            <Card>{projects[1]}</Card>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={1.8}
-            speed={1}
-          >
-            <Card>{projects[2]}</Card>
-          </ParallaxLayer>
       </Parallax>
 
-      <SliderHome open={open} setOpen={setOpen} />
+      <SliderHome open={open} setOpen={setOpen}/>
     </>
   );
 }
--- components/card.js
+++ components/card.js
@@ -15,7 +15,7 @@ export function Card({ href, className, data, children, disabled, ...props }) {
       <div className="flex flex-col justify-between space-y-4">
         <div className="[&>p]:text-slate-600 space-y-2 [&>h3]:!mt-0 [&>h4]:!mt-0">
           <div className="flex flex-row items-center justify-center gap-4">
-            <Image src={children.img} alt="Picture of bobbyHR" />
+            <Image src={children.img} className="w-48" alt="Picture of bobbyHR" />
             <div className="flex flex-col">
               <h3 className="text-2xl font-bold text-white">{children.title}</h3>
               <p className="text-sm font-medium text-white">{children.year}</p>
